ead144b6159ce3e8ae2fea7984aa5df67c1161cc
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-02 14:20:58 +0530

    add user property to Express Request interface

--- server/src/core/types/express.d.ts
+++ server/src/core/types/express.d.ts
@@ -3,6 +3,9 @@ export {};
 declare global {
   namespace Express {
     interface Request {
+      user?: {
+        id: string;
+      };
       validated?: {
         body: unknown;
         query: unknown;
