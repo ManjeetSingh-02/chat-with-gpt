db26ce083c98a9860127fe0c3b2fd8be845818e8
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-03 23:58:16 +0530

    refactor: integrate auth middleware directly into application setup and remove unused auth module

--- server/src/app/express/application.ts
+++ server/src/app/express/application.ts
@@ -1,26 +1,28 @@
 // internal-imports
-import { corsConfig, errorHandler, loadModules } from '@/core/index.js';
+import { auth, corsConfig, errorHandler, loadModules } from '@/core/index.js';
 
 // external-imports
 import cors from 'cors';
 import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
 
 // function to create application
 export default async function createApp() {
   // create express application
   const application = express();
 
-  // attach cors middlewares
-  application.use(cors(corsConfig));
+  // attach middlewares
+  application
+    .use(cors(corsConfig))
+    .all('/api/v1/auth/{*any}', toNodeHandler(auth))
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }));
 
   // load all modules
   await loadModules(application);
 
-  // attach other middlewares
-  application
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(errorHandler);
+  // attach error handler
+  application.use(errorHandler);
 
   // return the application
   return application;
