3a3eaae458059f46d3058e32e4683eae5198e77d
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-08 04:37:03 +0530

    tanstack query added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,22 @@ import { routeTree } from '@/routeTree.gen';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createRouter({ routeTree });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 1000,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
