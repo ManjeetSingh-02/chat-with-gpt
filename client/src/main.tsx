e830371a53074e6d57721b179e1131ed9c3c1612
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-09 02:06:52 +0530

    feat: add theme context and provider for theme management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import '@/index.css';
 import { routeTree } from '@/routeTree.gen';
+import { ThemeProvider } from '@/providers/theme-provider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
@@ -18,7 +19,9 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );
