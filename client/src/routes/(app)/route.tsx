39da8d48b45d515e3f39f7d1b01513d6c8d3666e
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-13 02:16:38 +0530

    feat: enhance route component to include user and conversations data

--- client/src/routes/(app)/route.tsx
+++ client/src/routes/(app)/route.tsx
@@ -5,13 +5,36 @@ import { createFileRoute, Outlet, redirect } from '@tanstack/react-router';
 export const Route = createFileRoute('/(app)')({
   beforeLoad: async ({ context }) => {
     if (!context.session) throw redirect({ to: '/' });
+    return { session: context.session };
   },
-  component: () => (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Outlet />
-      </SidebarInset>
-    </SidebarProvider>
-  ),
+  loader: async ({ context }) => {
+    return {
+      conversations: [],
+      user: {
+        image: context.session.user.image,
+        name: context.session.user.name,
+        email: context.session.user.email,
+      },
+    };
+  },
+  component: function Layout() {
+    const { conversations, user } = Route.useLoaderData();
+
+    return (
+      <SidebarProvider>
+        <AppSidebar
+          user={user}
+          groups={[
+            {
+              label: 'Recents',
+              conversations,
+            },
+          ]}
+        />
+        <SidebarInset>
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
+    );
+  },
 });
