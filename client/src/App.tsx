d425179d9f4bcc9e4e2f1db8907fa1c75347f2b7
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-09-14 00:19:53 +0530

    feat: move queryClient initialization to a separate module for better organization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,10 @@
 import { Spinner } from '@/components/ui/spinner';
 import { authClient } from '@/lib/auth-client';
+import { queryClient } from '@/lib/query';
 import { ThemeProvider } from '@/providers/theme-provider';
 import { routeTree } from '@/routeTree.gen';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-
-// create a new queryClient instance
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 30 * 1000,
-    },
-  },
-});
 
 // Create a new router instance
 const router = createRouter({
